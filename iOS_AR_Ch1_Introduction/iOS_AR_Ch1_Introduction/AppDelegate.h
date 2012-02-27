be426cbd9e4c78ffe3e2fdb6ecfed61cf4c19bca
risinsun <risinsun@lycos.co.kr>
2012-02-28 00:01:06 +0900

    Adding implementation files

---
+++ iOS_AR_Ch1_Introduction/iOS_AR_Ch1_Introduction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS_AR_Ch1_Introduction
+//
+//  Created by  on 12. 2. 27..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
