cd5adf5e766dea0267705b3724ec4c9a17f25569
risinsun <risinsun@lycos.co.kr>
2012-02-28 00:01:31 +0900

    Adding supporting files

---
+++ iOS_AR_Ch1_Introduction/iOS_AR_Ch1_Introduction/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_AR_Ch1_Introduction
+//
+//  Created by  on 12. 2. 27..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
